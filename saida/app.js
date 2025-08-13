e6c76165a5d5d4ae8ef577ca207223306170e35d
Gustavo Martins <gmartinsbus@gmail.com>
2025-08-13 15:29:57 -0300

    removendo arquivos dispensáveis

--- saida/app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const button = document.querySelector('.button');
-    button.addEventListener('click', () => {
-        button.textContent = "Você Pressionou o botão!";
-        button.style.backgroundColor = 'green';
-    });
-});
-export {};
-//# sourceMappingURL=app.js.map
