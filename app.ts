3618c9b6b47a7184783c79a0f338361e1af9b8fe
Gustavo Martins <gmartinsbus@gmail.com>
2025-08-13 17:10:20 -0300

    Conteudo Principal + estilo

--- app.ts
+++ app.ts
@@ -1,10 +1,17 @@
 
 
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.querySelector('.button') as HTMLButtonElement;
+    const text = document.getElementById('text') as HTMLParagraphElement;
 
     button.addEventListener('click', () => {
         button.textContent = "Você Pressionou o botão!";
         button.style.backgroundColor = 'green';
+        if (text) {
+        text.textContent = "Parabéns, você clicou no botão!";
+        text.style.color = 'brown';
+        }
     });
 });
